889f64e45d622d0484ebb63c910183406c184ac0
Gaston Khouri <gastonlozano2016@yahoo.com>
2022-01-21 16:36:56 -0400

    Primer commit

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from 'react';
+import { Product, onChangeArgs, InitialValues } from '../interfaces/interfaces';
+
+interface useProductArgs {
+    product: Product;
+    onChange?: ( args: onChangeArgs ) => void;
+    value?: number;
+    initialValues?: InitialValues;
+}
+
+const useProduct = ( { onChange, product, value = 0, initialValues }: useProductArgs ) => {
+
+    const [ counter, setCounter ] = useState<number>( initialValues?.count || value );
+
+    const isMounted = useRef( false );
+
+    useEffect( () => {
+
+        if ( !isMounted.current ) return;
+
+        setCounter( value );
+
+    }, [ value ] );
+
+    useEffect( () => {
+
+        isMounted.current = true;
+
+    }, [] );
+
+    const increaseBy = ( value: number ) => {
+
+        let newValue = Math.max( counter + value, 0 );
+
+        if ( initialValues?.maxCount ) {
+            newValue = Math.min( newValue, initialValues?.maxCount );
+        }
+
+        setCounter( newValue );
+
+        onChange && onChange( { count: newValue, product } );
+    };
+
+    const reset = () => {
+
+        setCounter( initialValues?.count || value );
+
+    };
+
+    return {
+        counter,
+        increaseBy,
+        reset,
+        isMaxCountReached: !!initialValues?.maxCount && initialValues?.maxCount === counter,
+        maxCount: initialValues?.maxCount
+    };
+
+};
+
+export default useProduct;
